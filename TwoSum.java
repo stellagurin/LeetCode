de771e70002bf507272cdabe469c679db9ceeb11
Stella Gurin <59928550+stellagurin@users.noreply.github.com>
2020-09-02 17:31:18 -0400

    Updated TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,11 @@
 class TwoSum {
+    /**
+      * @param nums array of numbers in which two numbers will reach a target sum
+      * @param target sum to find from array
+      * @return int array of two indexes where the sum of the numbers equals target sum
+      */
     public int[] twoSum(int[] nums, int target) {
+        // assume each nums array will have exactly one solution
         int[] indexSum = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
